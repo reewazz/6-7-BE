4cfcfd2a8719d4303cb5f956e573044a8eef9f0a
Riktriz <riwaj.neupane15@gmail.com>
2026-08-26 20:57:35 +0600

    login added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -27,4 +27,42 @@ export const createUser = async(req,res)=> {
 
 
 
+export const login = async (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+
+    const user = await User.findOne({email})
+
+    if (!user) {
+        return res.status(404).json({
+            message : "User not found"
+        })
+    }
+    
+// password123 -> dehduhw93242232398ru20ru2dehdfw
+    const isMatch = await bcrypt.compare(password,user.password)
+   
+    if(!isMatch) {
+        return res.status(500).json({
+            message: "Email or password incorrect"
+        })
+    }
+
+
+    const finalResponse = {
+        message : "Logged in successfully",
+        email : user.email,
+        fullName : user.fullName,
+        image : user.image,
+        id :user._id
+    }
+
+
+    res.json(finalResponse)
+
+
+}
+
+
 // hello ->cceuchgwei327->diuwedibwe->eiwhbfybwef
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { createUser } from "../controllers/userControllers.js"
+import { createUser, login } from "../controllers/userControllers.js"
 
 // const app = express()
 const router = express.Router()
 
 
 router.post("/create",createUser)
+router.post("/login",login)
 
 
 
